a8992a391657f7c469e6f1b10e4e17b79f77117d
Ваше Имя <korobko1371@yandex.ru>
2018-07-30 16:27:48 +0300

    make it better

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/*',function(req, res, next) {
 });
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27018/todo_db", { useNewUrlParser: true }, function (err){
+mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost:27018/todo_db", { useNewUrlParser: true }, function (err){
   if (err){
     console.log('mongo connection failed!');
   }
